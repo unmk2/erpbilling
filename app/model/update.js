25950802525a1e425b9d91d933ddc596b903358b
Alexandre Sousa <sousa.akira@gmail.com>
2018-08-07 11:32:42 -0300

    Update Finalizado

--- app/model/update.js
+++ app/model/update.js
@@ -1,28 +1,19 @@
-module.exports.ExeUpdate = (app, base, dados,termos)=>{
+module.exports.ExeUpdate = (app, base, dados, termos) => {
     var conn = app.config.dbSyncSql();
 
     var obj_key = Object.keys(dados);
-    console.log(obj_key);
-
     var keys = obj_key.join(',');
-    console.log(keys);
     var campos = [];
     obj_key.forEach(files => {
-        var valor = dados[files] + "'";
+        var valor = dados[files];
 
-        campos.push(valor);
-        console.log(valor);
+        var linhas = files + '=' + "'" + valor + "'";
+        campos.push(linhas);
+
         //console.log(dados.nome);
     })
-    console.log(campos);
-    var resultado = "'" + campos.join("," + "'");
+    var resultado = campos.join(",");
 
-    console.log(resultado.split(','));
-    console.log(resultado);
-
-    //var sql = `UPDATE ${base} SET ${keys} ${termos} `;
-
-    //console.log(sql);
-
-    //return qr_result = conn.query(sql); 
+    var sql = `UPDATE ${base} SET ${resultado} ${termos} `;
+    return conn.query(sql);
 }
