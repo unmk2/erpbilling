1081e871d75df7e21763cb3b70b30a257269435f
Jonas <instjns@outlook.com>
2018-08-26 18:12:31 -0300

    Create e Dell de "parametros" esta ok.

--- app/controlles/parametros.js
+++ app/controlles/parametros.js
@@ -1,14 +1,20 @@
 module.exports.ReaderParametros = (app,parametros = ``) =>{
     return app.app.model.reader.ExeReader(app, `configuracoes`, parametros);
 }
-module.exports.CreateParametros = (app,dados) => {
+module.exports.Create = (app,dados) => {
     
-    return app.app.model.create.ExeCreate(app,dados);
-}
-module.exports.DellParametros = (app,id) => {
-
+    return app.app.model.create.ExeCreate(app,`configuracoes`,dados);
 }
 
-module.exports.EditParametros = (app,id) => {
+/* Regra para deletar */
+module.exports.Dell = (app,id)=>{
+    return app.app.model.delete.ExeDelete(app,`configuracoes`,`WHERE id = '${id}'`);
+}
 
+module.exports.Edit = (app,id) => {
+
+}
+module.exports.Update = (app,dados,id) =>{
+    
+    return app.app.model.update.ExeUpdate(app, modulo, dados, `WHERE id = '${id}'`);
 }
